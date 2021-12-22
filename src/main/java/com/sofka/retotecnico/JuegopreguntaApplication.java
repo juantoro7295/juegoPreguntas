e9d78cdd0af02227df574f27ffef043621f2639c
juan pablo toro hurtado <juan.toro7295@uco.net.co>
2021-12-22 11:55:27 -0500

    se sube el proyecto base

---
+++ src/main/java/com/sofka/retotecnico/JuegopreguntaApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.retotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegopreguntaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegopreguntaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/retotecnico/JuegopreguntaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.retotecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegopreguntaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
